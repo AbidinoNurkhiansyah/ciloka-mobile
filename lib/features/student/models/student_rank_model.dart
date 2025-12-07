cf5498462661e0cbc0db0d44bc17da4a79b8e72c
Abidino Nurkhiansyah <abidinonurkhiansyah@gmail.com>
2025-12-07 21:37:41 +0700

    feat(leaderboard) : add studentRankModel for using model data in leaderboard student

---
+++ lib/features/student/models/student_rank_model.dart
@@ -0,0 +1,13 @@
+class StudentRankModel {
+  final String studentId;
+  final String studentName;
+  final String photoUrl;
+  final int level;
+
+  StudentRankModel({
+    required this.studentId,
+    required this.studentName,
+    required this.photoUrl,
+    required this.level,
+  });
+}
